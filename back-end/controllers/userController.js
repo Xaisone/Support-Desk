fa65a5b785aedbfc016f1bed7b551fa53a058c86
Xaisone <143376940+Xaisone@users.noreply.github.com>
2025-05-13 12:27:48 +0700

    124. Register User

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,4 +1,7 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs') 
+
+const User = require('../models/userModel')
 
 // @desc Register a new user
 // @route /api/users
@@ -9,12 +12,38 @@ const registerUser = asyncHandler( async(req, res) =>{
     //Validation
     if(!name || !email || !password){
    res.status(400)
-   throw new Error('Please inclue all fields') // ✅ asyncHandler จะจัดการส่ง response ให้
-   // หรือแบบชัดเจน:
-   // return res.status(400).json({ message: 'Please include all fields' })
+   throw new Error('Please inclue all fields') 
 }
 
-    res.send('Register Route');
+    // Find if user already exists
+    const userExists = await User.findOne({email});
+    if(userExists){
+        res.status(400);
+        throw new Error('User already exists')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400);
+        throw new error('Invalid user data')
+    }
+
 })
 
 // @desc Login a new user
