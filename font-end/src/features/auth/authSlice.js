b6e7ad52411437ff4776e519a5bad28bb56e0379
Xaisone <143376940+Xaisone@users.noreply.github.com>
2025-05-14 17:24:16 +0700

    131.Hook Register Form to Redux

--- font-end/src/features/auth/authSlice.js
+++ font-end/src/features/auth/authSlice.js
@@ -1,20 +1,28 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: null,
-    isError: false,
-    isSuccess: false,
-    isLoading: false,
-    message: ''
-}
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// Register new user
+export const register = createAsyncThunk("auth/register", async (user, thuckAPI) => {
+     console.log(user)
+});
+
+// Login user
+export const login = createAsyncThunk("auth/register", async (user, thuckAPI) => {
+     console.log(user)
+});
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) =>{
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
 
-    }
-})
-
-export default authSlice.reducer
+export default authSlice.reducer;
